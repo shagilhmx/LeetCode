d600e86c3474c2fb975cae345d170b79c91f6710
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-04 08:13:56 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -9,32 +9,23 @@ class Solution
 	    vector<string> ans;
 		vector<string>find_permutation(string S)
 		{
-		    string ds = "";
-		    int freq[S.length()];
-		    
-		    for(int i=0;i<S.length();i++) freq[i] = 0;
-		    
-		    helper(S, freq, ds);
+		    helper(0, S);
 		    
 		    sort(ans.begin(), ans.end());
 		    
 		    return ans;
 		}
 		
-		void helper(string s, int *freq, string ds) {
-		    if(ds.length() == s.length()) {
-		        ans.push_back(ds);
+		void helper(int index ,string s) {
+		    if(index == s.length()) {
+		        ans.push_back(s);
 		        return;
 		    }
 		    
-		    for(int i=0;i<s.length();i++) {
-		        if(!freq[i]) {
-		            ds += s[i];
-		            freq[i] = 1;
-		            helper(s, freq, ds);
-		            freq[i] = 0;
-		            ds.pop_back();
-		        }
+		    for(int i=index;i<s.length();i++) {
+		        swap(s[index], s[i]);
+		        helper(index + 1, s);
+		        swap(s[index], s[i]);
 		    }
 		}
 };
