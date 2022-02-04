a090ce83b6e8e3dff9085af95db128b7c68ebe49
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-04 11:41:15 +0530

    Time: 20 ms (37.42%), Space: 10.5 MB (27.48%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -2,26 +2,30 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        unordered_map<int, int> hmap;
+        vector<int> ds;
         
-        helper(0, nums);
+        for(int i=0;i<nums.size();i++) hmap[nums[i]]++;
         
-        sort(ans.begin(), ans.end());
-        
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        helper(hmap, ds, nums.size());
         
         return ans;
     }
     
-    void helper(int index, vector<int>& nums) {
-        if(index == nums.size()) {
-            ans.push_back(nums);
+    void helper(unordered_map<int, int>& hmap, vector<int> ds, int size) {
+        if(ds.size() == size) {
+            ans.push_back(ds);
             return;
         }
         
-        for(int i=index;i<nums.size();i++) {
-            swap(nums[index], nums[i]);
-            helper(index + 1, nums);
-            swap(nums[index], nums[i]);
+        for(auto it=hmap.begin();it!=hmap.end();it++) {
+            if(it->second > 0) {
+                ds.push_back(it->first);
+                hmap[it->first]--;
+                helper(hmap, ds, size);
+                hmap[it->first]++;
+                ds.pop_back();
+            }
         }
     }
 };
