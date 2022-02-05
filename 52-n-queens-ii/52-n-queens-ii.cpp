0b38bc5a04f7c970fa522820b75047a2b487ecda
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-05 16:26:21 +0530

    Time: 4 ms (81.49%), Space: 7.1 MB (26.63%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -2,32 +2,25 @@ class Solution {
 public:
     int count = 0;
     int totalNQueens(int n) {
-        vector<string> board(n);
-        
-        string s(n, '.');
-        
-        for(int i=0;i<n;i++) board[i] = s;
-        
         vector<int> leftRow(n, 0), upperDiagonal(2*n - 1, 0), lowerDiagonal(2*n - 1, 0);
-        helper(0, board, leftRow, upperDiagonal, lowerDiagonal, n);
+        
+        helper(0, leftRow, upperDiagonal, lowerDiagonal, n);
         
         return count;
     }
     
-    void helper(int col, vector<string>& board, vector<int> leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal, int n) {
+    void helper(int col, vector<int> leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal, int n) {
         if(col == n) {
             count++;
             return;
         }
         
         for(int row=0;row<n;row++) {
-            if(leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
-                board[row][col] = 'Q';
+            if(!leftRow[row] && !lowerDiagonal[row + col] && !upperDiagonal[n - 1 + col - row]) {
                 leftRow[row] = 1;
                 lowerDiagonal[row + col] = 1;
                 upperDiagonal[n - 1 + col - row] = 1;
-                helper(col + 1, board, leftRow, upperDiagonal, lowerDiagonal, n);
-                board[row][col] = '.';
+                helper(col + 1, leftRow, upperDiagonal, lowerDiagonal, n);
                 leftRow[row] = 0;
                 lowerDiagonal[row + col] = 0;
                 upperDiagonal[n - 1 + col - row] = 0;
