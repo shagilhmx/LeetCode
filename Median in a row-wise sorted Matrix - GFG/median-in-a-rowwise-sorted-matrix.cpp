0a29db8c7537eab9b3172707d950a0bdddb86ecd
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-07 20:56:36 +0530

    Added solution - LeetHub

--- Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -11,15 +11,30 @@ using namespace std;
 class Solution{   
 public:
     int median(vector<vector<int>> &matrix, int r, int c){
-        vector<int> v;
+        int low = 1, high = 1e9;
         
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++)
-                v.push_back(matrix[i][j]);
-                
-        sort(v.begin(), v.end());
+        while(low <= high) {
+            int mid = (low + high)>>1;
+            int cnt = 0; //<=mid
+            
+            for(int i=0;i<matrix.size();i++) cnt += countSamllerThanEqualToMid(matrix[i], mid);
+            
+            if(cnt <= (r*c)/2) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+    
+    int countSamllerThanEqualToMid(vector<int>& matrix, int mid) {
+        int l = 0, h = matrix.size()-1;
         
-        return v[v.size()/2];
+        while(l <= h) {
+            int md = (l + h)>>1;
+            
+            if(matrix[md] <= mid) l = md + 1;
+            else h = md - 1;
+        }
+        return l;
     }
 };
 
