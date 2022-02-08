6e131df252603c3ae4a616f07f48e04ff839ac1c
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-08 15:59:37 +0530

    Time: 7 ms (90.06%), Space: 10.1 MB (41.85%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int, int> umap;
-        for(auto elem : nums) umap[elem]++;
-        
-        for(auto it : umap) {
-            if(it.second == 1) ans.push_back(it.first);
+        vector<int> ans(2);
+        unsigned int x = 0;
+        int ones = 0, twos = 0;
+        for(auto i : nums) x ^= i;
+        x = (x ^ (x-1)) & x;
+        for(auto elem : nums) {
+            if(x & elem) ans[0] ^= elem;
+            else ans[1] ^= elem;
         }
         return ans;
     }
