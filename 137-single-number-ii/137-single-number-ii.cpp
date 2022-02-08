ff06a22275aef291a6b80ab0f0ad0213456c93b8
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-08 15:32:53 +0530

    Time: 11 ms (57.40%), Space: 9.5 MB (88.10%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        if(nums.size() < 2) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[nums.size()-2] != nums[nums.size()-1]) return nums[nums.size()-1];
+        int ones = 0, twos = 0;
         
-        int jump = 1;
-        while(jump <= nums.size()-1) {
-            if(nums[jump-1] == nums[jump]) jump += 3;
-            else return nums[jump-1];
+        for(auto elem : nums) {
+            ones = (ones ^ elem) & (~twos);
+            twos = (twos ^ elem) & (~ones);
         }
-        return nums[0];
+        return ones;
     }
 };
