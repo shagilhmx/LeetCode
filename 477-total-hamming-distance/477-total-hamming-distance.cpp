3f6b712e059a6f8452788c69c392c26b971bc124
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-10 15:57:06 +0530

    Time: 92 ms (24.33%), Space: 19.2 MB (8.72%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -3,6 +3,8 @@ public:
     int totalHammingDistance(vector<int>& nums) {
         vector<vector<int>> hmap(32, vector<int>(2, 0));
         
+        // to check the bits are on or off, elem & (1 << i) == 0 then off, otherwise if 1 then on.
+        
         for(int i=0;i<32;++i) {
             for(int elem : nums) {
                 auto temp = (elem & (1 << i));
