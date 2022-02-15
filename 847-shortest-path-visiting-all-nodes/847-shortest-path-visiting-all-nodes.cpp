14f4ce3f74238a8fe7b9875537735a1f41be9d1e
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-02-15 14:06:59 +0530

    Time: 153 ms (35.85%), Space: 18.2 MB (45.80%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -13,11 +13,14 @@ public:
     
     int helper(vector<vector<int>>& graph) {
         int totalNodes = graph.size();
-        int endMask = (1 << totalNodes) - 1; // 1111, the node at when we know that we have covered all the nodes.
+        int endMask = (1 << totalNodes) - 1; 
+        /* 1111, the node at when we know that we have covered all the nodes.*/
         
-        queue<pair<int, pair<int, int>>> q; // holds three values, value of node, dist we took and bitmask.
+        queue<pair<int, pair<int, int>>> q; 
+        /* holds three values, value of node, dist we took and bitmask.*/
         
-        set<pair<int, int>> vis; //visited to check we have certain node again.
+        set<pair<int, int>> vis; 
+        /*visited to check we have certain node again.*/
         
         for(int i=0;i<totalNodes;i++) {
             int mask = (1 << i); // 2^i;
@@ -31,11 +34,14 @@ public:
             int val = node.first, dist = node.second.first, mask = node.second.second;
             
             for(auto temp : graph[val]) { //now will be visiting all the child nodes of the val.
-                int newMask = (mask | (1 << temp)); /* now at starting if we have node 0
-                then the mask for 0 -> 0001 and now I come to node 1 -> 0010, then we have
+                int newMask = (mask | (1 << temp)); 
+                /* now at starting if we have node 0
+                then the mask for 0 -> 0001 and now 
+                I come to node 1 -> 0010, then we have
                 newMask as 0001 | 0010 = 0011*/
                 if(newMask == endMask) return dist + 1; //if we are at end node;
-                else if(vis.count({temp, newMask})) continue; //if we already visisted a node before with same mask.
+                else if(vis.count({temp, newMask})) continue; 
+                /*if we already visisted a node before with same mask.*/
                 
                 vis.insert({temp, newMask});
                 q.push({temp, {dist + 1, newMask}});
