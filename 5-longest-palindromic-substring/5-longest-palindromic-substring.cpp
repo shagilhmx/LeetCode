02449b8a40e879dbe642608cf0d6adf13eeb7f0c
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-05 00:07:29 +0530

    Time: 324 ms (37.02%), Space: 8.2 MB (69.05%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+        //using gap method
         int n = s.length();
+        int start = 0, end = 0;
+        
         bool dp[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=0;
-                if(i==j) dp[i][j]=1;
-                if(j==i+1 && s[i]==s[j]) dp[i][j]=1;
-            }
-        }
-        for(int i=2;i<n;i++){
-            int r=0,col=i;
-            while(col<n){
-                if(s[r]==s[col] && dp[r+1][col-1]==1) dp[r][col]=1;
-                r++;
-                col++;
-            }
-        }
-        int start=0,end=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(dp[i][j]==1){
-                    if(j-i>end-start){
-                        start=i;
-                        end=j;
+        
+        for(int gap=0;gap<n;gap++) {
+            for(int i=0, j=gap;j<n;i++, j++) {
+                if(gap == 0) dp[i][j] = true; //if only one character, i.e gap is zero.
+                /* if two character, i.e 
+                gap is 1, we check if both the character are same if yes return 2, otherwise                 1.*/
+                else if(gap == 1) dp[i][j] = s[i] == s[j] ? true : false; 
+                else {
+                    if(s[i] == s[j] && dp[i+1][j-1]) {
+                        dp[i][j] = true; //2 + midPart(str).
                     }
+                    else {
+                        dp[i][j] = false; // max(c1m, mc2).
+                    }
+                }
+                if(dp[i][j]) {
+                    start = i;
+                    end = j - i + 1;
                 }
             }
         }
-        
-        return s.substr(start,end-start+1);
+        return s.substr(start, end);
     }
 };
