3d1e16e069ea6cf04b964fc5fda1896775d0e5d0
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-14 22:13:10 +0530

    Time: 356 ms (77.43%), Space: 13.1 MB (31.86%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -1,83 +1,71 @@
-class node{
-public:
-    char c;
-    unordered_map<char, node*> children;
-    bool isEnd;
-    string word;
-    
-    node(char c){
-        this->c= c;
-        isEnd= false;
-        word="";
-    }
-};
-
-class trie{
-public:
-    node* root;
-    trie(){
-        this->root= new node('\0');
-    }
-    
-    void addWord(string s){
-        node* curr= root;
-        int n= s.length();
-        for(int i=0; i<n; i++){
-            char ch= s[i];
-            if(curr->children[ch] == NULL){
-                curr->children[ch]= new node(ch);
-            }
-            curr= curr->children[ch];
-        }
-        curr->isEnd= true;
-        curr->word= s;
-    }
-};
-
 class Solution {
-public:
-    trie t;
+struct Node {
+    char c;
+    int ends;
+    Node *link[26];
+    string word;
+};
     
-    void dfs(vector<vector<char>> &board, node* curr, int i, int j, vector<string> &ans){
+    struct Node *getNode(char ch) {
+        Node *newNode = new Node;
+        newNode -> c = ch;
+        newNode -> ends = 0;
+        newNode -> word = "";
+        for(int i=0;i<26;i++) newNode -> link[i] = NULL;
         
-        char c= board[i][j];
-        if(c == '.' or curr->children[c] == nullptr)
-            return;
-        
-        curr= curr->children[c];
-        if(curr->isEnd == true){
-            ans.push_back(curr->word);
-            curr->isEnd= false;
-        }
-        
-        c= board[i][j];
-        board[i][j]= '.';
-        
-        if(i>0) 
-            dfs(board, curr, i-1, j, ans);
-        if(i<board.size()-1)
-            dfs(board, curr, i+1, j, ans);
-        if(j>0)
-            dfs(board, curr, i, j-1, ans);
-        if(j<board[0].size()-1)
-            dfs(board, curr, i, j+1, ans);
-        
-        board[i][j]= c;
+        return newNode;
     }
     
+    Node *root = getNode('/');
+    
+    void insert(string s) {
+        Node *currNode = root;
+        int index, i = 0;
+        
+        while(s[i]) {
+            index = s[i] - 'a';
+            if(currNode -> link[index] == NULL) currNode -> link[index] = getNode(s[i]);
+            
+            currNode = currNode -> link[index];
+            i++;
+        }
+        currNode -> ends = 1;
+        currNode -> word = s;
+    }
+    
+    void solve(vector<vector<char>>& board, int i, int j, int n, int m, vector<string>& ans, Node *curr) {
+        int index = board[i][j] - 'a';
+        if(board[i][j] == '#' || curr -> link[index] == NULL) return;
+        
+        curr = curr -> link[index];
+        if(curr -> ends > 0) {
+            ans.push_back(curr -> word);
+            curr -> ends -= 1;
+        }
+        
+        char ch = board[i][j];
+        board[i][j] = '#';
+        
+        if(i > 0) solve(board, i - 1, j, n, m, ans, curr);
+        if(i < n - 1) solve(board, i + 1, j, n, m, ans, curr);
+        if(j > 0) solve(board, i, j - 1, n, m, ans, curr);
+        if(j < m - 1) solve(board, i, j + 1, n, m, ans, curr);
+        
+        board[i][j] = ch;
+    }
+    
+public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        int m= board.size();
-        int n= board[0].size();
+        int n = board.size();
+        int m = board[0].size();
+        
+        for(int i=0;i<words.size();i++) insert(words[i]);
+        
         vector<string> ans;
         
-        //insert all words into trie
-        for(auto s: words){
-            t.addWord(s);
-        }
-        //iterate over board to find if current word matches in the trie
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                dfs(board, t.root, i, j, ans);
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                solve(board, i, j, n, m, ans, root);
             }
         }
         return ans;
