0fdfa4d662d23c6bd22c94ddd5562616b20443e1
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-15 15:24:54 +0530

    Time: 3 ms (91.49%), Space: 7.8 MB (94.80%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -10,6 +10,12 @@ public:
         int n = costs.size();
         struct scheduling arr[n];
         
+        /*
+        we have to get the max profit i.e costs[i][1] - cost[i][0] should be max,
+        sort the array using this assumption and then sum up the first n/2 from costs[i][0]
+        and the next remaning from cost[i][1].
+        */
+        
         for(int i=0;i<n;i++) arr[i].costA = costs[i][0], arr[i].costB = costs[i][1], arr[i].profit = costs[i][1] - costs[i][0];
         
         sort(arr, arr + n, comparator);
