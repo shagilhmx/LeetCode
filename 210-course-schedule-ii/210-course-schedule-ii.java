4d614f0f9cc16582e731df4f317e08ab529cf8b6
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-24 17:29:29 +0530

    Time: 3 ms (98.94%), Space: 43.1 MB (91.42%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,34 +1,61 @@
 class Solution {
-public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegree(numCourses, 0);
-        vector<int> adjList[numCourses];
+    Stack<Integer> st = new Stack<>();
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        //using dfs.
+        List<Integer>[] graph = new ArrayList[numCourses];
         
-        for(int i=0;i<prerequisites.size();i++) {
-            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+        for(int i = 0; i < numCourses; i++) graph[i] = new ArrayList();
+        
+        // build adjecency list matrix
+        for(int[] ordering : prerequisites) {
+            graph[ordering[0]].add(ordering[1]);
         }
         
-        queue<int> q;
+        // boolean array of visited courses
+        boolean[] visited = new boolean[numCourses];
         
-        for(int i=0;i<numCourses;i++)
-            if(!indegree[i]) q.push(i);
+        int[] ans = new int[numCourses];
         
-        if(q.size() == 0) return {};
-        
-        vector<int> ans;
-            
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            
-            for(auto u : adjList[node]) {
-                indegree[u]--;
-                if(!indegree[u]) q.push(u);
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                if(!cycleDetection(graph, i, visited, new boolean[numCourses])) {
+                    return new int[0];
+                }
             }
         }
-        if(ans.size() != numCourses) return {};
+        
+        for(int i=numCourses-1;i>=0;i--) {
+            ans[i] = st.peek();
+            st.pop();
+        }
+        
         return ans;
     }
-};
+    
+    boolean cycleDetection(List<Integer>[] graph, int course, boolean[] visited, boolean[] onStack) {
+        visited[course] = true;
+        onStack[course] = true;
+        
+        for(int num : graph[course]) {
+            // if this child is already on stack
+            // this means a cycle exists
+            // return false
+            if(onStack[num]) return false;
+            
+            // if have not visited num yet,
+            // recursively run algorithm
+            if(!visited[num]) {
+                if(!cycleDetection(graph, num, visited, onStack)) {
+                    return false;
+                }
+            }
+        }
+        
+        // reset the course on the stack
+        // to false 
+        onStack[course] = false;
+        st.push(course);
+        
+        return true;
+    }
+}
