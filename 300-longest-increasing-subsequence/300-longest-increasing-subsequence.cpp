e69b5ba1f0056115e6f0f86143349260a33f84ef
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-25 23:55:59 +0530

    Time: 121 ms (9.83%), Space: 44.5 MB (37.26%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        //using dp
-        int n = nums.size();
-        vector<int> dp(n, -1);
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
         
         int res = 1;
         for(int i=0;i<n;i++)
-            res = max(res, helper(nums, dp, i));
+            res = Math.max(res, helper(nums, dp, i));
         
         return res;
     }
     
-    int helper(vector<int>& nums, vector<int>& dp, int index) {
-        if(index == nums.size()-1) {
+    int helper(int[] nums, int[] dp, int index) {
+        if(index == nums.length-1) {
             dp[index] = 1;
             return 1;
         }
@@ -22,11 +21,11 @@ public:
         
         int res = 1;
         
-        for(int i=index+1;i<nums.size();i++) 
+        for(int i=index+1;i<nums.length;i++) 
             if(nums[i] > nums[index])
-                res = max(res, 1 + helper(nums, dp, i));
+                res = Math.max(res, 1 + helper(nums, dp, i));
         
         dp[index] = res;
         return res;
     }
-};
+}
