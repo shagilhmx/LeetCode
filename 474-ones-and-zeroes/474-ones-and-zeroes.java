d82fb01913eaaf44ca3cdf40b32f4b454aca8aaa
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-26 12:53:27 +0530

    Time: 110 ms (38.06%), Space: 42.4 MB (77.09%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -16,23 +16,21 @@ class Solution {
                 dp[i][j] = 1;
         
         for(int i=1;i<r;i++) {
-            int[][] temp = new int[m+1][n+1];
             zero = one = 0; //counting number of zeros and ones.
             for(int j=0;j<strs[i].length();j++) {
                 if(strs[i].charAt(j) == '0') zero++;
                 else one++;
             }
             
-            for(int j=0;j<=m;j++) {
-                for(int k=0;k<=n;k++) {
+            for(int j=m;j>=0;j--) {
+                for(int k=n;k>=0;k--) {
                     int noPick = dp[j][k];
                     int pick = Integer.MIN_VALUE;
                     if(j - zero >= 0 && k - one >= 0)
                         pick = dp[j-zero][k-one] + 1;
-                    temp[j][k] = Math.max(pick, noPick);
+                    dp[j][k] = Math.max(pick, noPick);
                 }
             }
-            dp = temp;
         }
         return dp[m][n];
     }
