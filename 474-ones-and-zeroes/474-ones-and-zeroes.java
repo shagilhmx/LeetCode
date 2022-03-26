04779d3c5649252da7f051c4a71b7306b04c08f5
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-26 12:14:00 +0530

    Time: 143 ms (28.93%), Space: 88.4 MB (25.92%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        int r = strs.length;
+        int[][][] dp = new int[r][m+1][n+1];
+        
+        for(int i=0;i<r;i++)
+            for(int j=0;j<=m;j++)
+                for(int k=0;k<=n;k++)
+                    dp[i][j][k] = 0;
+        
+        //count zero and one for 1st index.
+        int zero = 0, one = 0;
+        for(int i=0;i<strs[0].length();i++) {
+            if(strs[0].charAt(i) == '0') zero++;
+            else one++;
+        }
+        
+        //base case.
+        for(int i=zero;i<=m;i++)
+            for(int j=one;j<=n;j++)
+                dp[0][i][j] = 1;
+        
+        for(int i=1;i<r;i++) {
+            zero = one = 0; //counting number of zeros and ones.
+            for(int j=0;j<strs[i].length();j++) {
+                if(strs[i].charAt(j) == '0') zero++;
+                else one++;
+            }
+            
+            for(int j=0;j<=m;j++) {
+                for(int k=0;k<=n;k++) {
+                    int noPick = dp[i-1][j][k];
+                    int pick = Integer.MIN_VALUE;
+                    if(j - zero >= 0 && k - one >= 0)
+                        pick = dp[i-1][j-zero][k-one] + 1;
+                    dp[i][j][k] = Math.max(pick, noPick);
+                }
+            }
+        }
+        return dp[r-1][m][n];
+    }
+}
