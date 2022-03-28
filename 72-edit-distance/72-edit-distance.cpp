2ac35626fbf868bbc249216cbc900307311f95c1
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-03-28 22:15:14 +0530

    Time: 12 ms (75.61%), Space: 9 MB (54.81%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,32 +1,19 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
         
-        int[][] dp = new int[n + 1][m + 1];
-        for(int[] row : dp)
-            Arrays.fill(row, -1);
-        
-        return lcs(word1, word2, n, m, dp);
-    }
-    
-    int lcs(String word1, String word2, int n, int m, int[][] dp) {
-        if(n == 0 || m == 0) {
-            return n == 0 ? m : n;
-        }
-        
-        if(dp[n][m] != -1)
-            return dp[n][m];
-        
-        if(word1.charAt(n - 1) == word2.charAt(m - 1))
-            return dp[n][m] = lcs(word1, word2, n - 1, m - 1, dp);
-        
-        else {
-            int insert = 1 + lcs(word1, word2, n, m - 1, dp);
-            int replace = 1 + lcs(word1, word2, n - 1, m, dp);
-            int remove = 1 + lcs(word1, word2, n - 1, m - 1, dp);
-            dp[n][m] = Math.min(replace, Math.min(insert, remove));
+        for(int i=0;i<=n;i++) {
+            for(int j=0;j<=m;j++) {
+                if(i == 0 || j == 0)
+                    dp[i][j] = i == 0 ? j : i;
+                else if(word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));//insertion, deletion and replace.
+            }
         }
         return dp[n][m];
     }
-}
+};
