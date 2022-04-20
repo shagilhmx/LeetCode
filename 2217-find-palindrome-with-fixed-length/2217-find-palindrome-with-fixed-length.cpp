64dac4a3fca380bca9d3fd17fc648a08567d1999
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-20 23:38:46 +0530

    Time: 256 ms (48.75%), Space: 53.9 MB (62.75%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -27,3 +27,21 @@ public:
         return ans;
     }
 };
+
+/*
+we divide the len in to half, like if len = 4, then we use 10 - 99
+10 - 01
+11 - 11
+12 - 21
+
+we take lowerbound and upperbound as limit.
+after which we there is no palindrome.
+
+left half is lowerbound + queries[i] - 1.
+right half is reverse of leftbound
+
+add lefthalf + righthalf(intLength % 2).
+as we will have to remove odd number of zeros.
+
+if its out of range then add -1.
+*/
