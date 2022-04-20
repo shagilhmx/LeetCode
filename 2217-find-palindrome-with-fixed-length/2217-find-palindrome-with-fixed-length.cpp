2f415540458505f9f047110858186b3aa252348d
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-20 23:32:56 +0530

    Time: 238 ms (56.38%), Space: 54.2 MB (15.76%) - LeetHub

---
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<long long> kthPalindrome(vector<int>& queries, int intLength) {
+        int n = queries.size();
+        vector<long long> ans;
+        int i = 0;
+        
+        int defaultLength = (intLength + 1) / 2;
+        
+        int lowerBound = (long)pow(10, defaultLength - 1);
+        int upperBound = (long)pow(10, defaultLength) - 1;
+        
+        for(int q : queries) {
+            int totalCount = (upperBound - lowerBound) + 1;
+            
+            if(q <= totalCount) {
+                string leftHalf = to_string(lowerBound + q - 1);
+                string rightHalf = leftHalf;
+                reverse(rightHalf.begin(), rightHalf.end());
+                
+                long res = stol(leftHalf + rightHalf.substr(intLength % 2));
+                ans.push_back(res);
+            }
+            else
+                ans.push_back(-1);
+        }
+        
+        return ans;
+    }
+};
