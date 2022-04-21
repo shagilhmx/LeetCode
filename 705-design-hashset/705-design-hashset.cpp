579a076784f7302e7abfd7ae218cf71ce19ce511
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-21 20:01:00 +0530

    Time: 101 ms (81.03%), Space: 39.7 MB (93.85%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.cpp
@@ -0,0 +1,38 @@
+class MyHashSet {
+public:
+    MyHashSet() {}
+    ~MyHashSet() {}
+    
+    public: void add(int key) {
+        if(!contains(key))
+            hashSet[hash(key)].push_back(key);
+    }
+    
+    public: void remove(int key) {
+        auto pos = find(hashSet[hash(key)].begin(), hashSet[hash(key)].end(), key);
+        if(pos != hashSet[hash(key)].end())
+            hashSet[hash(key)].erase(pos);
+    }
+    
+    public: bool contains(int key) {
+        auto pos = find(hashSet[hash(key)].begin(), hashSet[hash(key)].end(), key);
+        if(pos != hashSet[hash(key)].end())
+            return true;
+        return false;
+    }
+    
+    private:
+        vector<int> hashSet[50];
+    
+    int hash(int key) {
+        return key % 50;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
