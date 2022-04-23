d67e9c5e9459b63494b9ad5fd4cffe213e9812c6
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-23 14:02:57 +0530

    Added solution - LeetHub

--- Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
+++ Minimum increment/decrement to make array non-Increasing - GFG/minimum-incrementdecrement-to-make-array-non-increasing.cpp
@@ -28,6 +28,19 @@ public:
         return steps;
     }
 };
+/*
+You have to make the current element a[i] lesser to all the previous elements in 0 -> (i-1)....
+so maintain a min heap and add elements in it until you find a bigger element than the min till now.....when it is encountered, you make it lesser or equal to min and increase number of operations
+
+for the input {3, 1, 5, 1}
+
+1. i = 0, queue is empty initially ....so a[0] added in PQ -> [3]
+2. i = 1, a[1] < pq.top ....so a[1] is added in PQ -> [3, 1]
+3. i = 2, a[2] > pq.top ... so we make a[2] = pq.top() by decreasing a[2] (5) by 4 to make it 1 (pq.top). PQ -> [3, 5] ... operations = 4
+4. i = 3 a[3] < pq.top.... so a[3] is added in PQ -> [3, 5, 1]
+
+return operations ... ie. 4
+*/
 
 // { Driver Code Starts.
 
