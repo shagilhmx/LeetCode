931ff589aac0c2a8f41753c27a20972da2923bd3
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-23 21:31:57 +0530

    Time: 156 ms (57.81%), Space: 30.7 MB (48.11%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int n = maze.size(), m = maze[0].size();
+        
+        vector<pair<int, int>> dir{{0,1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        queue<pair<int, int>> q;
+        q.push({entrance[0], entrance[1]});
+        visited[entrance[0]][entrance[1]] = true;
+        int steps = 1;
+        
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                auto front = q.front();
+                q.pop();
+                
+                for(auto d : dir) {
+                    int dx = front.first + d.first, dy = front.second + d.second;
+                    
+                    if(dx >= 0 && dx < n && dy >= 0 && dy < m && maze[dx][dy] == '.' && !visited[dx][dy]) {
+                        if(dx == 0 || dy == 0 || dx == n - 1 || dy == m - 1)
+                            return steps;
+                        q.push({dx, dy});
+                        visited[dx][dy] = true;
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
+    }
+};
