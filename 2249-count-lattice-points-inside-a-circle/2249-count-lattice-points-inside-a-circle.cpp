9cbe7dad922d5cab3a6364afd6b5143fb602bb0c
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-24 17:50:23 +0530

    Time: 1297 ms (45.45%), Space: 25.1 MB (18.18%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.cpp
@@ -7,14 +7,14 @@ public:
             int x = circle[0];
             int y = circle[1];
             int r = circle[2];
-            int rsqr = r*r;
+            int rSquare = r*r;
             
             for(int k=x-r;k<=x+r;k++) {
                 for(int h=y-r;h<=y+r;h++) {
-                    int xsqr = (x - k) * (x - k);
-                    int ysqr = (y - h) * (y - h);
+                    int xSquare = (x - k) * (x - k);
+                    int ySquare = (y - h) * (y - h);
                     
-                    if(xsqr + ysqr <= rsqr) {
+                    if(xSquare + ySquare <= rSquare) {
                         pair<int, int> p;
                         p.first = k;
                         p.second = h;
