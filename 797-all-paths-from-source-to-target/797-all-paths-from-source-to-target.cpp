2152bb993e9b82853af12f7e5f2f43de6e6a37ba
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-26 22:44:23 +0530

    Time: 44 ms (19.35%), Space: 20.8 MB (10.52%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -5,7 +5,8 @@ public:
         int n = graph.size();
         vector<int> ds;
         
-        dfs(graph, 0, n - 1, ds);
+        //dfs(graph, 0, n - 1, ds);
+        bfs(graph, 0, n - 1, ds);
         
         return ans;
     }
@@ -20,5 +21,28 @@ public:
         for(auto u : graph[start])
             dfs(graph, u, end, path);
         
+    }
+    
+    void bfs(vector<vector<int>>& graph, int start, int end, vector<int>& path) {
+        path.push_back(start);
+        
+        queue<vector<int>> q;
+        q.push(path);
+        
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            
+            int node = curr.back();
+            if(node == end)
+                ans.push_back(curr);
+            else {
+                for(auto &u : graph[node]) {
+                    vector<int> temp(curr);
+                    temp.push_back(u);
+                    q.push(temp);
+                }
+            }
+        }
     }
 };
