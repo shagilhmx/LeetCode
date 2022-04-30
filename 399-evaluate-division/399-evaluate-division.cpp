55c6498cf1001cf820390c65c8b55cd69892c273
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-30 12:50:23 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (74.56%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    //{start, {end, value}}
+    unordered_map<string, unordered_map<string, double>> adj;
+    unordered_set<string> vis;
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        int n = equations.size(), m = queries.size();
+        
+        vector<double> ans;
+        ans.reserve(m);
+        
+        for(int i=0;i<n;i++) {
+            adj[equations[i][0]][equations[i][1]] = values[i];
+            adj[equations[i][1]][equations[i][0]] = 1/values[i];
+        }
+        
+        for(int i=0;i<m;i++) {
+            if(adj.count(queries[i][0]) == 0 || adj.count(queries[i][1]) == 0) {
+                ans.push_back(-1);
+                continue;
+            }
+            
+            vis.insert(queries[i][0]);
+            ans.push_back(dfs(queries[i][0], queries[i][1], vis));
+            vis.clear();
+        }
+        
+        return ans;
+    }
+    
+    double dfs(const string &start, const string &end, unordered_set<string>& vis) {
+        if(start == end)
+            return 1;
+        
+        vis.insert(start);
+        
+        //check all adj edges.
+        for(const auto &[nxt, wt] : adj[start]) {
+            if(!vis.count(nxt)) {
+                double res = dfs(nxt, end, vis);
+                if(res != -1)
+                    return res * wt;
+            }
+        }
+        return -1;
+    }
+};
