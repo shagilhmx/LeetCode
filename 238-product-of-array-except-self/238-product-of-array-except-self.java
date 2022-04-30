dace52f2589f98dbb0257d1161b3f7bbb7f5cf00
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-04-30 17:45:02 +0530

    Time: 3 ms (40.40%), Space: 58.1 MB (30.17%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,23 +1,21 @@
 class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
         
-        //storing the prefix
-        int prefix = 1;
-        for(int i=0;i<n;i++) {
-            ans[i] = prefix;
-            prefix *= nums[i];
-        }
+        //prefix.
+        left[0] = 1;
+        for(int i=1;i<n;i++)
+            left[i] = left[i - 1] * nums[i - 1];
         
-        //storing the postfix from end to start.
-        int postfix = 1;
-        for(int i=n-1;i>=0;i--) {
-            ans[i] *= postfix;
-            postfix *= nums[i];
-        }
+        right[n - 1] = 1;
+        for(int i=n-2;i>=0;i--)
+            right[i] = right[i + 1] * nums[i + 1];
         
-        return ans;
+        for(int i=0;i<n;i++)
+            nums[i] = right[i] * left[i];
+        
+        return nums;
     }
-};
+}
