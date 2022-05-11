d399cd1970480d051f3499584820a12482db3577
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-11 11:23:52 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.38%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -4,9 +4,10 @@ public:
         int vowelsSize = 5;
         string vowels = "aeiou";
         
-        vector<vector<int>> dp(vowelsSize + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(vowelsSize, vector<int>(n + 1, -1));
         //return helper(vowels, 0, n);
-        return solve(vowels, 0, n, dp);
+        //return solve(vowels, 0, n, dp);
+        return dynamicPrograming(vowels, n , dp);
     }
     
     int helper(string vowels, int index, int end) {
@@ -36,5 +37,24 @@ public:
         int notPick = helper(vowels, index + 1, end);
         
         return dp[index][end] = pick + notPick;
+    }
+    
+    int dynamicPrograming(string vowels, int n, vector<vector<int>>& dp) {
+        for(int i=0;i<5;i++)
+            dp[i][0] = 1; //base case.
+        
+        for(int i=4;i>=0;i--) {
+            for(int j=1;j<=n;j++) {
+                int pick = 0, notPick = 0;
+                
+                pick = dp[i][j - 1];
+                if(i < 4)
+                    notPick = dp[i + 1][j];
+                
+                dp[i][j] = pick + notPick; 
+            }
+        }
+        
+        return dp[0][n]; // return all strings possible starting from 0 index and of n length
     }
 };
