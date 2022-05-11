1ab40b5083ef229d3b066966173a03949d8d237e
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-11 11:16:07 +0530

    Time: 1566 ms (5.11%), Space: 6.2 MB (22.78%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
+        int vowelsSize = 5;
         string vowels = "aeiou";
-        return helper(vowels, 0, n);
+        
+        vector<vector<int>> dp(vowelsSize + 1, vector<int>(n + 1, -1));
+        //return helper(vowels, 0, n);
+        return solve(vowels, 0, n, dp);
     }
     
     int helper(string vowels, int index, int end) {
@@ -16,5 +20,21 @@ public:
         int notPick = helper(vowels, index + 1, end);
         
         return pick + notPick;
+    }
+    
+    int solve(string vowels, int index, int end, vector<vector<int>>& dp) {
+        if(end == 0)
+            return 1;
+        
+        if(index == vowels.size())
+            return 0;
+        
+        if(dp[index][end] != -1)
+            return dp[index][end];
+        
+        int pick = helper(vowels, index, end - 1);
+        int notPick = helper(vowels, index + 1, end);
+        
+        return dp[index][end] = pick + notPick;
     }
 };
