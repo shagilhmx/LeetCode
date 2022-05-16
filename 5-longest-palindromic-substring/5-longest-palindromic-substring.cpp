3b2952e9af275df59ffab01b45a9087f88cfa022
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-16 20:35:44 +0530

    Time: 1120 ms (7.62%), Space: 387.1 MB (5.64%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        //using gap method
-        int n = s.length();
-        int start = 0, end = 0;
-        
-        bool dp[n][n];
-        
+        int n = s.length(), start = 0, end = 0, ans = INT_MIN;
+        vector<vector<int>> dp(n, vector<int>(n));
         for(int gap=0;gap<n;gap++) {
-            for(int i=0, j=gap;j<n;i++, j++) {
-                if(gap == 0) dp[i][j] = true; //if only one character, i.e gap is zero.
-                /* if two character, i.e 
-                gap is 1, we check if both the character are same if yes return 2, otherwise                 1.*/
-                else if(gap == 1) dp[i][j] = s[i] == s[j] ? true : false; 
-                else {
-                    if(s[i] == s[j] && dp[i+1][j-1]) {
-                        dp[i][j] = true; //2 + midPart(str).
-                    }
-                    else {
-                        dp[i][j] = false; // max(c1m, mc2).
-                    }
+            for(int i=0,j=gap;j<n;j++,i++) {
+                if(gap == 0)
+                    dp[i][j] = 1;
+                else if(gap == 1) {
+                    if(s[i] == s[j])
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = 0;
                 }
-                if(dp[i][j]) {
+                else {
+                    if(s[i] == s[j] && dp[i + 1][j - 1] == 1)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = 0;
+                }
+                
+                if(j - i > ans && dp[i][j] == 1) {
+                    ans = j - i;
                     start = i;
                     end = j - i + 1;
                 }
             }
         }
-        return s.substr(start, end);
+        
+        return s.substr(start,end);
     }
 };
