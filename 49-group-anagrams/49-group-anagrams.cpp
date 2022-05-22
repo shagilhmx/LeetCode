2bc09e78ef92ce81cba4f0767c274e57e086c492
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-22 19:15:36 +0530

    Time: 71 ms (25.24%), Space: 30.7 MB (7.15%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
+        map<vector<int>, vector<string>> mpp;
+        int n = strs.size();
         
-        map<vector<int>, vector<string>> hmap;
-                
-        for(auto word : strs) {
+        for(int i=0;i<n;i++) {
+            vector<int> mask(26, 0);
+            for(int ch : strs[i])
+                mask[ch - 'a']++;
             
-            vector<int> masking(26, 0);
-
-            for(auto ch : word) {
-                masking[ch - 'a']++;
-            }
-            
-            hmap[masking].push_back(word);
+            mpp[mask].push_back(strs[i]);
         }
         
-        for(auto data : hmap) ans.push_back(data.second);
+        vector<vector<string>> ans;
+        for(auto el : mpp)
+            ans.push_back(el.second);
         
         return ans;
     }
