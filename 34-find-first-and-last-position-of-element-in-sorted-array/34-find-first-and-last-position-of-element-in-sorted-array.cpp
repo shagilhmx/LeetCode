27d9ffd98284b7d975fc8c018a751fec7d064cef
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-22 12:57:00 +0530

    Time: 16 ms (22.39%), Space: 13.7 MB (64.65%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1, index = -1;;
-        vector<int> ans;
+        int low = 0, high = nums.size() - 1;
+        vector<int> ans(2, -1);
         
-        //for end
-        while(start <= end) {
-            int mid = (end + start) / 2;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
             
             if(nums[mid] == target) {
-                index = mid;
-                end = mid - 1;
+                ans[0] = mid;
+                high = mid - 1;
             }
-            else if(target > nums[mid])
-                start = mid + 1;
+            
+            else if(nums[mid] < target)
+                low = mid + 1;
+            
             else
-                end = mid - 1;
+                high = mid - 1;
         }
         
-        ans.push_back(index);
-        
-        //for start.
-        start = 0, end = nums.size() - 1, index = -1;
-        while(start <= end) {
-            int mid = (end + start) / 2;
+        low = 0, high = nums.size() - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
             
             if(nums[mid] == target) {
-                index = mid;
-                start = mid + 1;
+                ans[1] = mid;
+                low = mid + 1;
             }
-            else if(target > nums[mid])
-                start = mid + 1;
+            
+            else if(nums[mid] < target)
+                low = mid + 1;
+            
             else
-                end = mid - 1;
+                high = mid - 1;
         }
-        ans.push_back(index);
         
         return ans;
     }
