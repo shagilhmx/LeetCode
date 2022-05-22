bba516c861d3076d9a4135c0af0a31445de2f108
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-22 13:22:56 +0530

    Time: 36 ms (37.88%), Space: 20 MB (35.13%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        //for each rows.
-        for(int i=0;i<9;i++) {
-            unordered_set<char> set;
-            for(int j=0;j<9;j++) {
-                if(board[i][j] == '.') continue;
-                if(set.find(board[i][j]) != set.end())
+        int n = board.size(), m = board[0].size();
+        
+        for(int i=0;i<n;i++) {
+            unordered_set<char> mpp;
+            for(int j=0;j<m;j++) {
+                if(board[i][j] == '.')
+                    continue;
+                else if(mpp.count(board[i][j]))
                     return false;
-                set.insert(board[i][j]);
+                mpp.insert(board[i][j]);
             }
         }
         
-        //for each cols.
-        for(int i=0;i<9;i++) {
-            unordered_set<char> set;
-            for(int j=0;j<9;j++) {
-                if(board[j][i] == '.') continue;
-                if(set.find(board[j][i]) != set.end())
+        for(int i=0;i<n;i++) {
+            unordered_set<char> mpp;
+            for(int j=0;j<m;j++) {
+                if(board[j][i] == '.')
+                    continue;
+                else if(mpp.count(board[j][i]))
                     return false;
-                set.insert(board[j][i]);
+                mpp.insert(board[j][i]);
             }
         }
         
-        //for sub-block.
-        for(int i=0;i<9;i++) {
-            unordered_set<char> set;
-            for(int j=0;j<9;j++) {
+        for(int i=0;i<n;i++) {
+            unordered_set<char> mpp;
+            for(int j=0;j<m;j++) {
                 if(board[3 * (i / 3) + j / 3][3 * (i % 3) + j % 3] == '.')
                     continue;
-                if(set.find(board[3 * (i / 3) + j / 3][3 * (i % 3) + j % 3]) != set.end())
+                else if(mpp.count(board[3 * (i / 3) + j / 3][3 * (i % 3) + j % 3]))
                     return false;
-                set.insert(board[3 * (i / 3) + j / 3][3 * (i % 3) + j % 3]);
+                mpp.insert(board[3 * (i / 3) + j / 3][3 * (i % 3) + j % 3]);
             }
         }
+        
         return true;
     }
 };
