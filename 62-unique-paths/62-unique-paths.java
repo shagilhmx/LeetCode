a63aff99a55e6519cb8761332ab6d5c21d1c6d19
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-23 14:51:25 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (84.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,21 +1,12 @@
 class Solution {
-public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+    public int uniquePaths(int m, int n) {
+        int N = n + m - 2;
+        int r = Math.min(n, m) - 1;
         
-        return solve(0, 0, m, n, dp);
+        long res = 1;
+        for(int i=1;i<=r;++i)
+            res = res * (N--) / i;
+        
+        return (int)res;
     }
-    
-    int solve(int i, int j, int m, int n, vector<vector<int>>& dp) {
-        if(i > m || i < 0 || j > n || j < 0)
-            return 0;
-        
-        if(i == m - 1 && j == n - 1)
-            return 1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        return dp[i][j] = solve(i + 1, j ,m ,n, dp) + solve(i, j + 1, m, n, dp);
-    }
-};
+}
