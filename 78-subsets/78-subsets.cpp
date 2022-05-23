30174928d8782648208d891c01616b9a593581e2
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-23 16:35:05 +0530

    Time: 4 ms (50.93%), Space: 7.5 MB (44.25%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,18 @@
 class Solution {
-public:
     vector<vector<int>> ans;
+public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> ds;
-        
-        sort(nums.begin(), nums.end());
-        helper(0, nums, ds);
+        solve(0, nums, {});
         
         return ans;
     }
     
-    void helper(int index, vector<int>& nums, vector<int>& ds) {
+    void solve(int index, vector<int>& nums, vector<int> ds) {
         ans.push_back(ds);
         
         for(int i=index;i<nums.size();i++) {
-            if(i != index && nums[i] == nums[i-1]) continue;
             ds.push_back(nums[i]);
-            helper(i + 1, nums, ds);
+            solve(i + 1, nums, ds);
             ds.pop_back();
         }
     }
