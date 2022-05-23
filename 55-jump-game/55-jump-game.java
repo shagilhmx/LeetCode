f784795cb05ef3c590f7d3450b088a765120f052
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-23 10:11:20 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (88.94%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //using greedy approch.
-        /*we take a nums size value to reach to and start from end - 1 and check
-        if we can reach to end from end - 1 by adding the elem, if yes then we
-        make our goal to end - 1 - 1, which is end - 1 - i.
-        */
-        int toReach = nums.length - 1;
+        int n = nums.length;
         
-        for(int i=toReach-1;i>=0;i--) {
-            if(nums[i] + i >= toReach)
+        int toReach = n - 1;
+        for(int i=toReach;i>=0;i--)
+            if(i + nums[i] >= toReach)
                 toReach = i;
-        }
-        return (toReach == 0) ? true : false; 
+        
+        return toReach == 0 ? true : false;
     }
 }
