9613276c27e3eee5c9e5dfd82b64fa9dbe1ef099
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-23 10:09:00 +0530

    Time: 307 ms (20.48%), Space: 49.2 MB (14.85%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,23 +2,25 @@ class Solution {
     int dp[10001];
 public:
     bool canJump(vector<int>& nums) {
-        //using memoisation.
+        int n = nums.size() - 1;
         memset(dp, -1, sizeof(dp));
-        return helper(0, nums, nums.size()-1);
+        return solve(0, nums, n);
     }
     
-    bool helper(int index, vector<int>& nums, int n) {
-        if(index > n)
-            return false;
-        if(dp[index] != -1)
-            return dp[index];
+    bool solve(int index, vector<int>& nums, int n) {
         if(index == n)
             return dp[index] = true;
         
-        for(int i=nums[index];i>=1;i--) {
-            if(helper(index + i, nums, n))
+        if(index > n)
+            return false;
+        
+        if(dp[index] != -1)
+            return dp[index];
+        
+        for(int i=nums[index];i>=1;i--)
+            if(solve(index + i, nums, n))
                 return dp[index] = true;
-        }
+        
         return dp[index] = false;
     }
 };
