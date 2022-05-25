2f1f0985498e7f5cb187d9f5a1360d63c53228d6
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-25 17:04:18 +0530

    Time: 271 ms (86.29%), Space: 8 MB (36.79%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        //dfs solution.
-        for(int i=0;i<board.size();i++) {
-            for(int j=0;j<board[i].size();j++) {
-                if(dfs(board, word, i, j, 0)) return true;
-            }
-        }
+        for(int i=0;i<board.size();i++)
+            for(int j=0;j<board[0].size();j++)
+                if(dfs(board, i, j, word, 0))
+                    return true;
         return false;
     }
     
-    bool dfs(vector<vector<char>>& board, string word, int i, int j, int index) {
-        if(index >= word.size()) return true;
-        if(i < 0 || j < 0 || i >= board.size() || j >= board[i].size() || word[index] != board[i][j]) return false;
+    bool dfs(vector<vector<char>>& board, int i, int j, string& word, int index) {
+        if(index == word.size()) 
+            return true;
         
-        board[i][j] = '#';
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != word[index])
+            return false;
         
-        bool ans = dfs(board, word, i+1, j, index + 1) || dfs(board, word, i - 1, j, index + 1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1);
+        board[i][j] = '*';
+        bool res = dfs(board, i + 1, j, word, index + 1) || dfs(board, i - 1, j, word, index + 1) || dfs(board, i, j + 1, word, index + 1) || dfs(board, i, j - 1, word, index + 1);
         board[i][j] = word[index];
         
-        return ans;
+        return res;
     }
 };
