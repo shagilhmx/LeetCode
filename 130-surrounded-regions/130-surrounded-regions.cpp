5c55542dd03803ae979899a40b6c772d51398389
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-30 11:54:55 +0530

    Time: 24 ms (25.38%), Space: 10 MB (79.68%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -0,0 +1,55 @@
+class Solution {
+    vector<pair<int, int>> dir{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+public:
+    void solve(vector<vector<char>>& board) {
+        queue<pair<int, int>> q;
+        
+        if(board.size() == 0 && board[0].size() == 0)
+            return; //if size is 0 return.
+        
+        //check if its the border elem and mark it as '.' as we can mark it back to 'O' if its not connected.
+        for(int i=0;i<board.size();i++) {
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j] == 'O' && isBorder(i, j, board.size(), board[0].size())) {
+                    board[i][j] = '.';
+                    q.push({i, j});
+                }
+            }
+        }
+
+        //bfs to mark the middle and connected to 'X'.
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            
+            board[curr.first][curr.second] = '.';
+            
+            for(auto d : dir) {
+                int dx = d.first + curr.first;
+                int dy = d.second + curr.second;
+                
+                if(dx >= 0 && dx < board.size() && dy >= 0 && dy < board[0].size() && board[dx][dy] == 'O') {
+                    board[dx][dy] = '.';
+                    q.push({dx, dy});
+                }
+            }
+        }
+        
+        //marking the non connected end elems back to 'O' and all the others to 'X'.
+        for(int i=0;i<board.size();i++) {
+            for(int j=0;j<board[0].size();j++) {
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if(board[i][j] == '.')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+    
+    bool isBorder(int i, int j, int n, int m) {
+        //to check if its the border elem.
+        if(i == n - 1 || i == 0 || j == m - 1 || j == 0)
+            return true;
+        return false;
+    }
+};
