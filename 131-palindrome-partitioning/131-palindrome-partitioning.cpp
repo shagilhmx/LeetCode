8bce540e75869af5070e4c563acd018eaf3c108f
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-05-30 13:01:49 +0530

    Time: 345 ms (15.64%), Space: 163.2 MB (14.20%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -2,31 +2,37 @@ class Solution {
     vector<vector<string>> ans;
 public:
     vector<vector<string>> partition(string s) {
-        helper(0, {}, s);
+        vector<vector<int>> dp(s.length(), vector<int>(s.length()));
+        
+        for(int i=0;i<s.length();i++) {
+            for(int j=0;j<=i;j++) {
+                if(i - j <= 2)
+                    dp[i][j] = s[i] == s[j] ? true : false;
+                else {
+                    if(dp[i - 1][j + 1])
+                        dp[i][j] = s[i] == s[j] ? true : false;
+                }
+            }
+        }
+        
+        helper(0, {}, s, dp);
         
         return ans;
     }
     
-    void helper(int index, vector<string> ds, string& s) {
+    void helper(int index, vector<string> ds, string& s, vector<vector<int>>& dp) {
         if(index == s.length()) {
             ans.push_back(ds);
             return;
         }
         
         for(int i = index;i<s.length();i++) {
-            if(isPalindrome(s, index, i)) {
+            if(dp[i][index]) {
                 ds.push_back(s.substr(index, i - index + 1));
-                helper(i + 1, ds, s);
+                helper(i + 1, ds, s, dp);
                 ds.pop_back();
             }
         }
         return;
-    }
-    
-    bool isPalindrome(string s, int start, int end) {
-        while(start <= end)
-            if(s[start++] != s[end--])
-                return false;
-        return true;
     }
 };
