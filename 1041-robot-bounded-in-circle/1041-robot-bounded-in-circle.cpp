28017f30fba46c1578469edbeef3fc1d9131f0eb
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-06-18 18:24:29 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.39%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        pair<int, int> end;
+        end = {0, 0};
+        char currDir = 'N';
+        
+        for(auto each : instructions) {
+            switch(each) {
+                case 'G':
+                    switch(currDir) {
+                        case 'N':
+                            end.second++;
+                            break;
+                        case 'S':
+                            end.second--;
+                            break;
+                        case 'E':
+                            end.first++;
+                            break;
+                        case 'W':
+                            end.first--;
+                            break;
+                    }
+                    break;
+                case 'L':
+                  switch(currDir) {
+                        case 'N':
+                            currDir = 'W';
+                            break;
+                        case 'S':
+                            currDir = 'E';
+                            break;
+                        case 'E':
+                            currDir = 'N';
+                            break;
+                        case 'W':
+                            currDir = 'S';
+                            break;
+                    }
+                    break;  
+                case 'R':
+                  switch(currDir) {
+                        case 'N':
+                            currDir = 'E';
+                            break;
+                        case 'S':
+                            currDir = 'W';
+                            break;
+                        case 'E':
+                            currDir = 'S';
+                            break;
+                        case 'W':
+                            currDir = 'N';
+                            break;
+                    }
+                    break;
+            }
+        }
+        
+        return (originVerifier(end.first, end.second) || currDir != 'N') ? true : false;
+    }
+    
+    bool originVerifier(int x, int y) {
+        return (x == 0 && y == 0) ? true : false;
+    }
+};
