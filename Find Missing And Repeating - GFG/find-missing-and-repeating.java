105a58f23ddbc4c41110b1f6b0e6e76d6e74b5b4
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-07-09 13:33:01 +0530

    Added solution - LeetHub

---
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        int repeating = 0, missing = 0;
+        for(int i=0;i<n;i++) {
+            if(arr[Math.abs(arr[i]) - 1] > 0)
+                arr[Math.abs(arr[i]) - 1] = -arr[Math.abs(arr[i]) - 1];
+            else
+                repeating = Math.abs(arr[i]);
+        }
+        
+        for(int i=0;i<n;i++)
+            if(arr[i] > 0)
+                missing = i + 1;
+               
+        int []ans = new int[2];
+        ans[0] = repeating; ans[1] = missing;
+        
+        return ans;
+    }
+}
