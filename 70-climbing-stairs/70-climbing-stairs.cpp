6569dd50376f1adf027194ce5121e425c8f9aa3b
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-08-05 14:38:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (97.29%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int a = 1, b = 1;
+        int dp[n + 1];
+        dp[0] = 1, dp[1] = 1;
         
-        for(int i=2;i<=n;i++) {
-            int c = a + b;
-            a = b;
-            b = c;
-        }
+        for(int i=2;i<=n;i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
         
-        return b;
+        return dp[n];
     }
 };
