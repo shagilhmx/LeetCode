2aa4c166fbd55b6f6382d8309787168ef06d9a9a
Md Shagil Nizmai <33639955+shagilhmx@users.noreply.github.com>
2022-08-07 19:31:19 +0530

    Time: 86 ms (34.18%), Space: 35.8 MB (30.66%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int dp[50001];
     bool canReach(vector<int>& arr, int start) {
-        memset(dp, -1, sizeof(dp));
+        queue<int> q;
+        q.push(start);
         set<int> vis;
-        return solve(start, arr, vis);
-    }
-    
-    int solve(int start, vector<int>& arr, set<int>& vis) {
-        if(start < 0 || start >= arr.size())
-            return 0;
         
-        if(dp[start] != -1)
-            return dp[start];
+        while(!q.empty()) {
+            int top = q.front();
+            q.pop();
+            
+            if(arr[top] == 0)
+                return true;
+            
+            if(vis.count(top))
+                continue;
+            
+            vis.insert(top);
+            
+            if(top - arr[top] >= 0)
+                q.push(top - arr[top]);
+            
+            if(top + arr[top] < arr.size())
+                q.push(top + arr[top]);
+        }
         
-        if(vis.count(start))
-            return 0;
-        
-        vis.insert(start);
-        
-        if(arr[start] == 0)
-            return 1;
-        
-        return dp[start] = solve(start + arr[start], arr, vis) || solve(start - arr[start], arr, vis);
+        return false;
     }
 };
